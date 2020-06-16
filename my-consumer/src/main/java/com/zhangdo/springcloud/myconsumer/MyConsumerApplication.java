ff7e5c36a56a44c8f9efeca38f226ed4690e848b
Doris Zhang <willing_happy@163.com>
2020-06-16 11:54:14 +0800

    Initial commit

---
+++ my-consumer/src/main/java/com/zhangdo/springcloud/myconsumer/MyConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zhangdo.springcloud.myconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyConsumerApplication.class, args);
+    }
+
+}
---
+++ my-consumer/src/test/java/com/zhangdo/springcloud/myconsumer/MyConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangdo.springcloud.myconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
